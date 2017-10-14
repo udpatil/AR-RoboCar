489443b18a8d2b2b750db683372a6248905f6a01
Vishwa Shah <vshah0350@gmail.com>
2017-10-14 11:17:45 -0400

    camera with center overlay working

--- PuppetBot/app/src/main/java/me/udaypatil/puppetbot/MainActivity.java
+++ PuppetBot/app/src/main/java/me/udaypatil/puppetbot/MainActivity.java
@@ -9,5 +9,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (null == savedInstanceState) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, CameraFragment.newInstance())
+                    .commit();
+        }
     }
 }
